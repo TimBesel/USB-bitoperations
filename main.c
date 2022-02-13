3dc181cf7733995b04234a69aa4ac26c7f3b6531
Timothy <timothy.besel@googlemail.com>
2022-02-13 20:09:02 +0100

    add Non-Return-Zero-Inverted function

--- main.c
+++ main.c
@@ -6,13 +6,16 @@
 typedef uint8_t packet_typ_t;
 
 packet_typ_t *bit_stuffing(packet_typ_t *data, uint8_t dataSize);
+packet_typ_t *nrzi(packet_typ_t *data, uint8_t dataSize);
 void transfer(packet_typ_t *data, uint8_t dataSize);
 
 int main(void)
 {   
-    packet_typ_t rData[4] = {0xFF, 0xFF, 0xFF, 0xFF};
-    packet_typ_t *data = bit_stuffing(rData, 32);
-    transfer((data+1), data[0]);
+    packet_typ_t rData[1] = {0xFF};
+    packet_typ_t *data = bit_stuffing(rData, 8);
+    uint8_t dataSize = data[0];
+    data = nrzi(data+1, dataSize);
+    transfer(data, dataSize);
     return 0;
 }
 
@@ -47,10 +50,26 @@ packet_typ_t *bit_stuffing(packet_typ_t *data, uint8_t dataSize){
 packet_typ_t *nrzi(packet_typ_t *data, uint8_t dataSize){
     packet_typ_t *dataBuffer = (packet_typ_t *)malloc((size_t)(ceil(dataSize / (8 * sizeof(packet_typ_t)))) * sizeof(packet_typ_t));
     uint8_t coparator = 0x01;
-    
-    for(uint8_t bIdx = 0; bIdx < dataSize; bIdx++){
 
+    for(uint8_t bIdx = 0; bIdx < (dataSize - dataSize%8); bIdx++){
+         uint8_t bit = (*(data + (bIdx/(8 * sizeof(packet_typ_t)))) >> (7-(bIdx % (8 * sizeof(packet_typ_t))))) & 0x01;
+
+         if(bit == 0x00)
+            coparator = (~coparator & 0x01);
+
+        packet_typ_t *dataPointer = (dataBuffer + (bIdx / (8 * sizeof(packet_typ_t))));
+        *dataPointer = (packet_typ_t)((*dataPointer << 1) | coparator);
     }
+    for(uint8_t bIdx = 0; bIdx < dataSize%(8 * sizeof(packet_typ_t)); bIdx++){
+        uint8_t bit = (*(data + (uint8_t)ceil(dataSize/(8 * sizeof(packet_typ_t)))) >> ((dataSize % (8 * sizeof(packet_typ_t))) - (bIdx+1) )) & 0x01;
+
+        if(bit == 0x00)
+            coparator = (~coparator & 0x01);
+
+        packet_typ_t *dataPointer = (dataBuffer + (bIdx / (8 * sizeof(packet_typ_t))) + 1);
+        *dataPointer = (packet_typ_t)((*dataPointer << 1) | coparator);
+    }
+    return dataBuffer;
 }
 
 void transfer(packet_typ_t *data, uint8_t dataSize){
