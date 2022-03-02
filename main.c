6915844601539195a5b24255bd013b0e5a7fba1c
Timothy Besel <timothy.besel@googlemail.com>
2022-03-02 12:54:25 +0100

    fix some convenstion bugs

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -13,7 +14,7 @@ int main(void)
 {
     packet_typ_t rData[1] = {0xffffffff};
     packet_typ_t *data = bit_stuffing(rData, 32);
-    uint8_t dataSize = data[0];
+    uint8_t dataSize = (uint8_t)data[0];
     data = nrzi(data+1, dataSize);
     transfer(data, dataSize);
     return 0;
@@ -22,33 +23,33 @@ int main(void)
 packet_typ_t *bit_stuffing(packet_typ_t *data, uint8_t dataSize){
     uint8_t buffer = 0x00;
     uint8_t insertedBits = 0;
-    packet_typ_t *dataBuffer = (packet_typ_t *)malloc((size_t)(ceil(dataSize / (8 * sizeof(packet_typ_t))) + 1) * sizeof(packet_typ_t));
+    packet_typ_t *dataBuffer = (packet_typ_t *)malloc((size_t)(ceil((uint8_t)(dataSize / (8 * sizeof(packet_typ_t)))) + 1) * sizeof(packet_typ_t));
 
     for(uint8_t bIdx = 0; bIdx < dataSize; bIdx++){
 
         uint8_t bit = (*(data + (bIdx/(8 * sizeof(packet_typ_t)))) >> ((8 * sizeof(packet_typ_t) - 1) - (bIdx % (8 * sizeof(packet_typ_t))))) & 0x01;
-        buffer = (packet_typ_t)(buffer << 1) | (bit);
+        buffer = (uint8_t)(packet_typ_t)(buffer << 1) | (bit);
 
-        packet_typ_t *dataPointer = (dataBuffer + ((bIdx + insertedBits)/(8 * sizeof(packet_typ_t))) + 1);
+        packet_typ_t *dataPointer = (dataBuffer + ((uint8_t)(bIdx + insertedBits)/(8 * sizeof(packet_typ_t))) + 1);
         
         if((buffer & 0x7F) == 0x7F){
             buffer &= 0xFE;
             *dataPointer = (*dataPointer << 1) | (0x00);
-            buffer = (packet_typ_t)(buffer << 1) | (0x01);
-            dataBuffer = (packet_typ_t *)realloc(dataBuffer, ((size_t)ceil((dataSize + (++insertedBits)) / (8 * sizeof(packet_typ_t))) + 1) * sizeof(packet_typ_t));
-            dataPointer = (dataBuffer + ((bIdx + insertedBits)/(8 * sizeof(packet_typ_t))) + 1);
+            buffer = (uint8_t)(packet_typ_t)(buffer << 1) | (0x01);
+            dataBuffer = (packet_typ_t *)realloc(dataBuffer, ((size_t)ceil((uint8_t)((uint8_t)(dataSize + (++insertedBits)) / (8 * sizeof(packet_typ_t)))) + 1) * sizeof(packet_typ_t));
+            dataPointer = (dataBuffer + ((uint8_t)(bIdx + insertedBits)/(8 * sizeof(packet_typ_t))) + 1);
         }
         printf("%d\n", (buffer & 0x01));
         *dataPointer = (packet_typ_t)((*dataPointer << 1) | (buffer & 0x01));
 
     }
 
-    dataBuffer[0] = dataSize + insertedBits;
+    dataBuffer[0] = dataSize + (packet_typ_t)insertedBits;
     return dataBuffer;
 }
 
 packet_typ_t *nrzi(packet_typ_t *data, uint8_t dataSize){
-    packet_typ_t *dataBuffer = (packet_typ_t *)malloc((size_t)(ceil(dataSize / (8 * sizeof(packet_typ_t)))) * sizeof(packet_typ_t));
+    packet_typ_t *dataBuffer = (packet_typ_t *)malloc((size_t)(ceil((uint8_t)(dataSize / (8 * sizeof(packet_typ_t))))) * sizeof(packet_typ_t));
     uint8_t coparator = 0x01;
 
     for(uint8_t bIdx = 0; bIdx < (dataSize - dataSize%8); bIdx++){
@@ -61,7 +62,7 @@ packet_typ_t *nrzi(packet_typ_t *data, uint8_t dataSize){
         *dataPointer = (packet_typ_t)((*dataPointer << 1) | coparator);
     }
     for(uint8_t bIdx = 0; bIdx < dataSize%(8 * sizeof(packet_typ_t)); bIdx++){
-        uint8_t bit = (*(data + (uint8_t)ceil(dataSize/(8 * sizeof(packet_typ_t)))) >> ((dataSize % (8 * sizeof(packet_typ_t))) - (bIdx+1) )) & 0x01;
+        uint8_t bit = (*(data + (uint8_t)ceil((uint8_t)(dataSize/(8 * sizeof(packet_typ_t))))) >> ((dataSize % (8 * (uint8_t)sizeof(packet_typ_t))) - (bIdx+1) )) & 0x01;
 
         if(bit == 0x00)
             coparator = (~coparator & 0x01);
@@ -78,8 +79,7 @@ void transfer(packet_typ_t *data, uint8_t dataSize){
         printf("%u", bit);
     }
     for(uint8_t i = 0; i < dataSize%(8 * sizeof(packet_typ_t)); i++){
-        uint8_t bit = (*(data + (uint8_t)ceil(dataSize/(8 * sizeof(packet_typ_t)))) >> ((dataSize % (8 * sizeof(packet_typ_t))) - (i+1) )) & 0x01;
+        uint8_t bit = (*(data + (uint8_t)ceil((uint8_t)(dataSize/(8 * sizeof(packet_typ_t))))) >> ((dataSize % (8 * (uint8_t)sizeof(packet_typ_t))) - (i+1) )) & 0x01;
         printf("%u", bit);
     }
 }
-
