d453aead7b1fec65f2d16c8e8017360b15cd9b19
Timothy Besel <timothy.besel@googlemail.com>
2022-03-06 11:56:18 +0100

    fix dataSize type bug for longer messages

--- main.c
+++ main.c
@@ -5,17 +5,18 @@
 #include <string.h>
 
 typedef uint32_t packet_typ_t;
+typedef uint16_t dataSize_typ_t;
 
-packet_typ_t *bit_stuffing(packet_typ_t *data, uint8_t dataSize);
-packet_typ_t *nrzi(packet_typ_t *data, uint8_t dataSize);
-uint32_t crc(packet_typ_t *data, uint8_t dataSize, uint32_t pollynomial);
+packet_typ_t *bit_stuffing(packet_typ_t *data, dataSize_typ_t dataSize);
+packet_typ_t *nrzi(packet_typ_t *data, dataSize_typ_t dataSize);
+uint32_t crc(packet_typ_t *data, dataSize_typ_t dataSize, uint32_t pollynomial);
 uint8_t logOf2(uint32_t n);
-void transfer(packet_typ_t *data, uint8_t dataSize);
+void transfer(packet_typ_t *data, dataSize_typ_t dataSize);
 
 int main(void) {
-  packet_typ_t rData[1] = {0xFFFFFFFF};
-  packet_typ_t *data = bit_stuffing(rData, 32);
-  uint8_t dataSize = (uint8_t)data[0];
+  packet_typ_t rData[20] = {0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF};
+  packet_typ_t *data = bit_stuffing(rData, 640);
+  dataSize_typ_t dataSize = (dataSize_typ_t)data[0];
   data = nrzi(data + 1, dataSize);
   transfer(data, dataSize);
   uint16_t crcData = (uint16_t)crc(data, dataSize, 0x8005);
@@ -23,24 +24,24 @@ int main(void) {
   return 0;
 }
 
-packet_typ_t *bit_stuffing(packet_typ_t *data, uint8_t dataSize) {
+packet_typ_t *bit_stuffing(packet_typ_t *data, dataSize_typ_t dataSize) {
   uint8_t buffer = 0x00;
   uint8_t insertedBits = 0;
-  packet_typ_t *dataBuffer = (packet_typ_t *)malloc((size_t)(ceil((uint8_t)(dataSize / (8 * sizeof(packet_typ_t)))) + 1) * sizeof(packet_typ_t));
+  packet_typ_t *dataBuffer = (packet_typ_t *)malloc((size_t)(ceil((dataSize_typ_t)(dataSize / (8 * sizeof(packet_typ_t)))) + 1) * sizeof(packet_typ_t));
 
-  for (uint8_t bIdx = 0; bIdx < dataSize; bIdx++) {
+  for (dataSize_typ_t bIdx = 0; bIdx < dataSize; bIdx++) {
 
     uint8_t bit = (*(data + (bIdx / (8 * sizeof(packet_typ_t)))) >> ((8 * sizeof(packet_typ_t) - 1) - (bIdx % (8 * sizeof(packet_typ_t))))) & 0x01;
     buffer = (uint8_t)(packet_typ_t)(buffer << 1) | (bit);
 
-    packet_typ_t *dataPointer = (dataBuffer + ((uint8_t)(bIdx + insertedBits) / (8 * sizeof(packet_typ_t))) + 1);
+    packet_typ_t *dataPointer = (dataBuffer + ((dataSize_typ_t)(bIdx + insertedBits) / (8 * sizeof(packet_typ_t))) + 1);
 
     if ((buffer & 0x7F) == 0x7F) {
       buffer &= 0xFE;
       *dataPointer = (*dataPointer << 1) | (0x00);
       buffer = (uint8_t)(packet_typ_t)(buffer << 1) | (0x01);
-      dataBuffer = (packet_typ_t *)realloc(dataBuffer, ((size_t)ceil((uint8_t)((uint8_t)(dataSize + (++insertedBits)) / (8 * sizeof(packet_typ_t)))) + 1) * sizeof(packet_typ_t));
-      dataPointer = (dataBuffer + ((uint8_t)(bIdx + insertedBits) / (8 * sizeof(packet_typ_t))) + 1);
+      dataBuffer = (packet_typ_t *)realloc(dataBuffer, ((size_t)ceil((uint8_t)((dataSize_typ_t)(dataSize + (++insertedBits)) / (8 * sizeof(packet_typ_t)))) + 1) * sizeof(packet_typ_t));
+      dataPointer = (dataBuffer + ((dataSize_typ_t)(bIdx + insertedBits) / (8 * sizeof(packet_typ_t))) + 1);
     }
     *dataPointer = (packet_typ_t)((*dataPointer << 1) | (buffer & 0x01));
   }
@@ -49,11 +50,11 @@ packet_typ_t *bit_stuffing(packet_typ_t *data, uint8_t dataSize) {
   return dataBuffer;
 }
 
-packet_typ_t *nrzi(packet_typ_t *data, uint8_t dataSize) {
-  packet_typ_t *dataBuffer = (packet_typ_t *)malloc((size_t)(ceil((uint8_t)(dataSize / (8 * sizeof(packet_typ_t))))) * sizeof(packet_typ_t));
+packet_typ_t *nrzi(packet_typ_t *data, dataSize_typ_t dataSize) {
+  packet_typ_t *dataBuffer = (packet_typ_t *)malloc((size_t)(ceil((dataSize_typ_t)(dataSize / (8 * sizeof(packet_typ_t))))) * sizeof(packet_typ_t));
   uint8_t coparator = 0x01;
 
-  for (uint8_t bIdx = 0; bIdx < (dataSize - dataSize % 8); bIdx++) {
+  for (dataSize_typ_t bIdx = 0; bIdx < (dataSize - dataSize % 8); bIdx++) {
     uint8_t bit = (*(data + (bIdx / (8 * sizeof(packet_typ_t)))) >> ((8 * sizeof(packet_typ_t) - 1) - (bIdx % (8 * sizeof(packet_typ_t))))) & 0x01;
 
     if (bit == 0x00)
@@ -62,8 +63,8 @@ packet_typ_t *nrzi(packet_typ_t *data, uint8_t dataSize) {
     packet_typ_t *dataPointer = (dataBuffer + (bIdx / (8 * sizeof(packet_typ_t))));
     *dataPointer = (packet_typ_t)((*dataPointer << 1) | coparator);
   }
-  for (uint8_t bIdx = 0; bIdx < dataSize % (8 * sizeof(packet_typ_t)); bIdx++) {
-    uint8_t bit = (*(data + (uint8_t)ceil((uint8_t)(dataSize / (8 * sizeof(packet_typ_t))))) >> ((dataSize % (8 * (uint8_t)sizeof(packet_typ_t))) - (bIdx + 1))) & 0x01;
+  for (dataSize_typ_t bIdx = 0; bIdx < dataSize % (8 * sizeof(packet_typ_t)); bIdx++) {
+    uint8_t bit = (*(data + (uint8_t)ceil((dataSize_typ_t)(dataSize / (8 * sizeof(packet_typ_t))))) >> ((dataSize % (8 * (dataSize_typ_t)sizeof(packet_typ_t))) - (bIdx + 1))) & 0x01;
 
     if (bit == 0x00)
       coparator = (~coparator & 0x01);
@@ -75,12 +76,12 @@ packet_typ_t *nrzi(packet_typ_t *data, uint8_t dataSize) {
 }
 
 // up to crc32
-uint32_t crc(packet_typ_t *data, uint8_t dataSize, uint32_t pollynomial) {
+uint32_t crc(packet_typ_t *data, dataSize_typ_t dataSize, uint32_t pollynomial) {
   uint32_t buffer = 0x00;
   uint32_t crc = 0x00;
   uint8_t highesExp = logOf2(pollynomial);
 
-  for (uint8_t bIdx = 0; bIdx < (dataSize - dataSize % 8); bIdx++) {
+  for (dataSize_typ_t bIdx = 0; bIdx < (dataSize - dataSize % 8); bIdx++) {
     uint8_t bit = (*(data + (bIdx / (8 * sizeof(uint32_t)))) >> ((8 * sizeof(packet_typ_t) - 1) - (bIdx % (8 * sizeof(uint32_t))))) & 0x01;
     buffer = (uint32_t)(buffer << 1) | (bit);
 
@@ -90,7 +91,7 @@ uint32_t crc(packet_typ_t *data, uint8_t dataSize, uint32_t pollynomial) {
   }
 
   for (uint8_t bIdx = 0; bIdx < dataSize % (8 * sizeof(uint32_t)); bIdx++) {
-    uint8_t bit = (*(data + (uint8_t)ceil((uint8_t)(dataSize / (8 * sizeof(uint32_t))))) >> ((dataSize % (8 * (uint8_t)sizeof(uint32_t))) - (bIdx + 1))) & 0x01;
+    uint8_t bit = (*(data + (uint8_t)ceil((dataSize_typ_t)(dataSize / (8 * sizeof(uint32_t))))) >> ((dataSize % (8 * (dataSize_typ_t)sizeof(uint32_t))) - (bIdx + 1))) & 0x01;
     buffer = (uint32_t)(buffer << 1) | (bit);
 
     if ((buffer & (uint32_t)(1 << highesExp)) == (uint32_t)(1 << highesExp)) {
@@ -119,13 +120,13 @@ uint8_t logOf2(uint32_t n) {
   return counter;
 }
 
-void transfer(packet_typ_t *data, uint8_t dataSize) {
-  for (uint8_t i = 0; i < (dataSize - dataSize % 8); i++) {
+void transfer(packet_typ_t *data, dataSize_typ_t dataSize) {
+  for (dataSize_typ_t i = 0; i < (dataSize - dataSize % 8); i++) {
     uint8_t bit = (*(data + (i / (8 * sizeof(packet_typ_t)))) >> ((8 * sizeof(packet_typ_t) - 1) - (i % (8 * sizeof(packet_typ_t))))) & 0x01;
     printf("%u", bit);
   }
   for (uint8_t i = 0; i < dataSize % (8 * sizeof(packet_typ_t)); i++) {
-    uint8_t bit = (*(data + (uint8_t)ceil((uint8_t)(dataSize / (8 * sizeof(packet_typ_t))))) >> ((dataSize % (8 * (uint8_t)sizeof(packet_typ_t))) - (i + 1))) & 0x01;
+    uint8_t bit = (*(data + (dataSize_typ_t)ceil((dataSize_typ_t)(dataSize / (8 * sizeof(packet_typ_t))))) >> ((dataSize % (8 * (dataSize_typ_t)sizeof(packet_typ_t))) - (i + 1))) & 0x01;
     printf("%u", bit);
   }
   printf("\n");
